364abd1d5702bdb2c4093dab6044057a4c493a61
Alexey Timofeev <TImofeevAV8f@yandex.ru>
2026-01-13 18:30:29 +0300

    fix client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	endpoint := "http://localhost:8888/"
+	endpoint := "http://localhost:8080/"
 	// приглашение в консоли
 	fmt.Println("Введите длинный URL")
 	// открываем потоковое чтение из консоли
